a471942397558d2b76cca592aa449ebf8bb5e705
Peter Puhov <peterpuhov.github@gmail.com>
2021-01-08 10:48:57 -0500

    NN and DN redirection

--- dikeHDFS/src/dikeHDFS.cpp
+++ dikeHDFS/src/dikeHDFS.cpp
@@ -7,11 +7,14 @@
 #include <Poco/Net/HTTPServerRequest.h>
 #include <Poco/Net/HTTPServerResponse.h>
 
+#include <Poco/Net/HTTPClientSession.h>
+
 #include <Poco/Util/ServerApplication.h>
 #include <Poco/Util/AbstractConfiguration.h>
 #include <Poco/Util/XMLConfiguration.h>
 
-#include "Poco/StreamCopier.h"
+#include <Poco/StreamCopier.h>
+#include <Poco/URI.h>
 
 #include <iostream>
 
@@ -22,29 +25,85 @@ using namespace Poco::Net;
 using namespace Poco::Util;
 using namespace std;
 
-class ProxyHandler : public Poco::Net::HTTPRequestHandler {
+class NameNodeHandler : public Poco::Net::HTTPRequestHandler {
 public:  
    virtual void handleRequest(Poco::Net::HTTPServerRequest &req, Poco::Net::HTTPServerResponse &resp)
    {
-    cout << DikeUtil().Yellow() << DikeUtil().Now() << " Start " << DikeUtil().Reset();
-    cout << req.getURI() << endl;
-     
-    req.write(cout);
-    istream& istr = req.stream();
-    Poco::StreamCopier::copyStream(istr, std::cout);
-    cout << endl;
-
+    cout << DikeUtil().Yellow() << DikeUtil().Now() << " NN Start " << DikeUtil().Reset() << endl;
     HTTPRequest hdfs_req((HTTPRequest)req);
+    
+    string host = req.getHost();    
+    host = host.substr(0, host.find(':'));
+    hdfs_req.setHost(host, 9870);
+
+    cout << hdfs_req.getURI() << endl;     
     hdfs_req.write(cout);
-   }
-   
+
+    HTTPClientSession session(host, 9870);
+    session.sendRequest(hdfs_req);
+    HTTPResponse hdfs_resp;
+
+    std::istream& rs = session.receiveResponse(hdfs_resp);
+    
+    (HTTPResponse &)resp = hdfs_resp;    
+
+    cout << DikeUtil().Blue() << resp.get("Location") << DikeUtil().Reset() << endl;
+    string location = resp.get("Location");
+    location.replace(location.find(":9864"), 5, ":9859");
+    resp.set("Location", location);
+    cout << DikeUtil().Blue() << resp.get("Location") << DikeUtil().Reset() << endl;
+
+    resp.write(cout);
+    ostream& ostr = resp.send();
+    ostr.flush();
+  
+    cout << DikeUtil().Yellow() << DikeUtil().Now() << " NN End " << DikeUtil().Reset() << endl;
+   }   
 };
 
-class DikeRequestHandlerFactory : public HTTPRequestHandlerFactory {
+class DataNodeHandler : public Poco::Net::HTTPRequestHandler {
+public:  
+   virtual void handleRequest(Poco::Net::HTTPServerRequest &req, Poco::Net::HTTPServerResponse &resp)
+   {
+    cout << DikeUtil().Yellow() << DikeUtil().Now() << " DN Start " << DikeUtil().Reset() << endl;
+    HTTPRequest hdfs_req((HTTPRequest)req);
+    
+    string host = req.getHost();    
+    host = host.substr(0, host.find(':'));
+    hdfs_req.setHost(host, 9864);
+
+    cout << hdfs_req.getURI() << endl;     
+    hdfs_req.write(cout);
+
+    HTTPClientSession session(host, 9864);
+    session.sendRequest(hdfs_req);
+    HTTPResponse hdfs_resp;
+
+    std::istream& fromHDFS = session.receiveResponse(hdfs_resp);
+    
+    (HTTPResponse &)resp = hdfs_resp;    
+
+    resp.write(cout);
+    ostream& toClient = resp.send();
+
+    Poco::StreamCopier::copyStream(fromHDFS, toClient, 8192);
+    toClient.flush();
+
+    cout << DikeUtil().Yellow() << DikeUtil().Now() << " DN End " << DikeUtil().Reset() << endl;
+   }   
+};
+
+class NameNodeHandlerFactory : public HTTPRequestHandlerFactory {
 public:
   virtual HTTPRequestHandler* createRequestHandler(const HTTPServerRequest & req) {
+    return new NameNodeHandler;
+  }
+};
 
-    return new ProxyHandler;
+class DataNodeHandlerFactory : public HTTPRequestHandlerFactory {
+public:
+  virtual HTTPRequestHandler* createRequestHandler(const HTTPServerRequest & req) {
+    return new DataNodeHandler;
   }
 };
 
@@ -53,15 +112,19 @@ class DikeServerApp : public ServerApplication
 protected:
   int main(const vector<string> &)
   {
-    HTTPServer s(new DikeRequestHandlerFactory, ServerSocket(9860), new HTTPServerParams);
+    HTTPServer nameNode(new NameNodeHandlerFactory, ServerSocket(9860), new HTTPServerParams);
+    HTTPServer dataNode(new DataNodeHandlerFactory, ServerSocket(9859), new HTTPServerParams);
 
-    s.start();
+    nameNode.start();
+    dataNode.start();
+
     cout << endl << "Server started" << endl;
 
     waitForTerminationRequest();  // wait for CTRL-C or kill
 
     cout << endl << "Shutting down..." << endl;
-    s.stop();
+    nameNode.stop();
+    dataNode.stop();
 
     return Application::EXIT_OK;
   }
