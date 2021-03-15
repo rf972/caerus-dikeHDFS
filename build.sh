8f2b2e5ebf8512c5d19db31366f8183fb8276073
Robert Foley <robfoley972@gmail.com>
2021-03-15 14:38:55 -0400

    Minor update to build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ git submodule update --recursive --progress
 cd docker
 ./build-docker.sh || (echo "*** dikeHDFS build-docker failed with $?" ; exit 1)
 cd ..
-./build_hdfs_server.sh || (echo "*** hdfs server build failed with $?" ; exit 1)
+./build_hdfs_server.sh ./build.sh || (echo "*** hdfs server build failed with $?" ; exit 1)
 ./build_dike_server.sh || (echo "*** dike server build failed with $?" ; exit 1)
 ./build_ndp_client.sh || (echo "*** ndp client build failed with $?" ; exit 1)
 
