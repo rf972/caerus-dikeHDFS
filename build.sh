247e1f9ae748023381381435268c2e3cda0ea6d9
Robert Foley <robfoley972@gmail.com>
2021-03-15 09:21:00 -0400

    Added top level build script and script to build ndp client.

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+git submodule init
+git submodule update --recursive --progress
+
+cd docker
+./build-docker.sh || (echo "*** dikeHDFS build-docker failed with $?" ; exit 1)
+cd ..
+./build_hdfs_server.sh || (echo "*** hdfs server build failed with $?" ; exit 1)
+./build_dike_server.sh || (echo "*** dike server build failed with $?" ; exit 1)
+./build_ndp_client.sh || (echo "*** ndp client build failed with $?" ; exit 1)
+
+
